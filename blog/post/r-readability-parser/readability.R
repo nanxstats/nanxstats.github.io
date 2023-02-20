1d2a500c2859ee0e62785e55437c9f9336f18a3d
Nan Xiao <me@nanx.me>
2023-02-19 20:53:58 -0500

    Shorten blog post title to fit in single lines

--- blog/post/r-readability-parser/readability.R
+++ blog/post/r-readability-parser/readability.R
@@ -15,14 +15,14 @@ readability <- function(html, url, candidates = 5L, threshold = 500L) {
 
 is_readable <- function(html, min_content_length = 140, min_score = 20) {
   ct <- V8::v8(global = "window")
-  
+
   ct$eval("function setTimeout(){}")
   ct$eval("function clearInterval(){}")
   ct$source("js/encoding.min.js")
   ct$source("js/jsdom.js")
   ct$source("js/readability.js")
   ct$eval(readLines("js/readability-parser.js"))
-  
+
   # ct$get(V8::JS("Object.keys(window)"))
   ct$call("isReadable", html, min_content_length, min_score)
 }
